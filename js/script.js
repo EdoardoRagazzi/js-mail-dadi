8a0df5783071d8a5de9089431d2540b63cd1429f
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-05-28 14:24:39 +0200

    END

--- js/script.js
+++ js/script.js
@@ -6,4 +6,17 @@ var pcRandom = Math.floor(Math.random() * 6) + 1;
 console.log(manRandom);
 console.log(pcRandom);
 
+// Adesso che abbiamo generato i due numeri random, stabiliamo il vincitore confrontando i due numeri:
+
+if (manRandom == pcRandom) {
+
+    document.getElementById('winner').innerHTML = '' + 'Draw';
+
+} else if (manRandom < pcRandom) {
+
+    document.getElementById('winner').innerHTML = '' + 'The Winner is the Pc';
+} else {
+
+    document.getElementById('winner').innerHTML = '' + 'The Winner is the Man';
+}
 
